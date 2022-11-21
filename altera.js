3d786a315b2b47c00b6092085645ea8f4e7c06c0
Holivane <holivane@gmail.com>
2022-11-21 00:05:18 -0300

    alterando chaves de um objeto

---
+++ altera.js
@@ -0,0 +1,14 @@
+const pessoa = {
+  nome: "Laura",
+  profissao: "professora",
+}
+
+console.log(pessoa.nome);
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 922223344"
+console.log(pessoa.telefone);
+
+pessoa.nome = "Laura Silva";
+
+console.log(pessoa);
