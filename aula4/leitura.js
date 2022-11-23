0cfbc44b5ef50e8dce43f05ac1fab119ce36f1f5
Holivane <holivane@gmail.com>
2022-11-22 22:53:28 -0300

    Stringify

--- aula4/leitura.js
+++ aula4/leitura.js
@@ -2,4 +2,10 @@ const dados = require('./cliente.json');
 
 console.log(dados);
 
-console.log(typeof(dados));
+console.log('Tipo: ',typeof(dados), '\n');
+
+const clienteEmString = JSON.stringify(dados);
+
+console.log(clienteEmString);
+
+console.log('Tipo: ' , typeof(clienteEmString));
