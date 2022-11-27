e173abab26abd57b827f87cb4f991934deb8a449
Holivane <holivane@gmail.com>
2022-11-27 20:53:38 -0300

    Processando diretórios

--- aula6-nodejs/src/cli.js
+++ aula6-nodejs/src/cli.js
@@ -1,11 +1,28 @@
 import chalk from "chalk";
+import fs from 'fs';
 import getFile from "./index.js";
 
 const way = process.argv;
 
-async function processText(way) {
-  const result = await getFile(way[2]);
+function printList (result) {
   console.log(chalk.yellow('Lista de links: '), result);
 }
 
+async function processText(req) {
+  const way = req[2];
+
+  if (fs.lstatSync(way).isFile()) {
+    const result = await getFile(way);
+    printList(result);
+  } else if (fs.lstatSync(way).isDirectory()) {
+    const files = await fs.promises.readdir(way)
+    files.forEach(async (fileName) => {
+      const list = await getFile(`${way}/${fileName}`);
+      printList(list);
+    })
+    console.log(files);
+  }
+}
+
 processText(way);
+ 
