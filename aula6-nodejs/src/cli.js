f01ab00649cc46b92ad81c66bd0d196fce8f1f65
Holivane <holivane@gmail.com>
2022-11-27 20:16:26 -0300

    Organização das saídas

--- aula6-nodejs/src/cli.js
+++ aula6-nodejs/src/cli.js
@@ -1,5 +1,11 @@
+import chalk from "chalk";
 import getFile from "./index.js";
 
 const way = process.argv;
 
-getFile(way[2])
+async function processText(way) {
+  const result = await getFile(way[2]);
+  console.log(chalk.yellow('Lista de links: '), result);
+}
+
+processText(way);
--- aula6-nodejs/src/index.js
+++ aula6-nodejs/src/index.js
@@ -10,14 +10,14 @@ function extractLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
   const parts = [...texto.matchAll(regex)];
   const result = parts.map(part => ({[part[1]]: part[2]}));
-  return result;
+  return result.length !== 0 ? result : 'Não há links no arquivo';
 }
 
 async function getFile(filePath) {
   try {
     const encoding = 'utf-8';
     const text = await fs.promises.readFile(filePath, encoding);
-    console.log(extractLinks(text));
+    return extractLinks(text);
   } catch (error){
     handleError(error)
   } 
