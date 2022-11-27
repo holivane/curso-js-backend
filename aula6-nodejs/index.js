44652a7c621a46d56d4bbb775cfdd229e107952c
Holivane <holivane@gmail.com>
2022-11-27 15:51:51 -0300

    fs e tratamento de erros

--- aula6-nodejs/index.js
+++ aula6-nodejs/index.js
@@ -1,1 +1,19 @@
-console.log('Olá mundo')
+import fs from 'fs'
+import chalk from 'chalk';
+
+function handleError(error) {
+  console.log(error);
+  throw new Error(chalk.red(error.code, 'Não há este caminho'));
+}
+
+function getFile(filePath) {
+  const encoding = 'utf-8';
+  fs.readFile(filePath, encoding, (error, res) => {
+    if (error) {
+      handleError(error);
+    }
+    console.log(chalk.green(res));
+  })
+}
+
+getFile('./arquivos/texto.md')
