54da4e38c0e45b023770528141d8c2e4d314c598
Holivane <holivane@gmail.com>
2022-11-27 21:10:27 -0300

    tratamento de novos erros

--- aula6-nodejs/src/cli.js
+++ aula6-nodejs/src/cli.js
@@ -4,21 +4,30 @@ import getFile from "./index.js";
 
 const way = process.argv;
 
-function printList (result) {
-  console.log(chalk.yellow('Lista de links: '), result);
+function printList (result, file) {
+  console.log(chalk.yellow(`Lista de links do arquivo: ${file} `), result);
 }
 
 async function processText(req) {
   const way = req[2];
 
+  try {
+    fs.lstatSync(way)
+  } catch (error) {
+    if(error.code === 'ENOENT') {
+      console.log(chalk.red('Arquivo ou diretório não existente'))
+      return;
+    }
+  }
+
   if (fs.lstatSync(way).isFile()) {
     const result = await getFile(way);
-    printList(result);
+    printList(result, way);
   } else if (fs.lstatSync(way).isDirectory()) {
     const files = await fs.promises.readdir(way)
     files.forEach(async (fileName) => {
       const list = await getFile(`${way}/${fileName}`);
-      printList(list);
+      printList(list, fileName);
     })
     console.log(files);
   }
