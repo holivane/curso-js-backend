3518243a2ce44c7868414962ffafc605b9c586a0
Holivane <holivane@gmail.com>
2022-12-05 07:27:07 -0300

    Criada e aplicada função 'verificaSe'.

--- aula7-nodejs-testes-unitarios-integracao/index.js
+++ aula7-nodejs-testes-unitarios-integracao/index.js
@@ -1,17 +1,37 @@
+import assert from 'node:assert/strict';
+
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
+const verificaSe = (valor) => {
+  const assercoes = {
+    ehExatemanteIgualA(esperado) {
+      assert.strictEqual(valor, esperado, 'O valor retornado não é o esperado');
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
     console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
+  } catch (error) {
+    console.log(`${titulo} não passou`, error);
   }
 };
 
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+teste('somaHorasExtras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
 
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
+  verificaSe(retornado).ehExatemanteIgualA(esperado);
+});
 
-teste('calculaDesconto', 2200, calculaDescontos(2500, 100));
+teste('calculaDesconto', () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verificaSe(retornado).ehExatemanteIgualA(esperado);
+});
