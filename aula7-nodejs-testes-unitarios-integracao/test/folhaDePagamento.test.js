e3ef851d57e5056a89660013d9a9e1ad12f0be95
Holivane <holivane@gmail.com>
2022-12-17 02:15:36 -0300

    Criados testes de folha de pagamento

--- aula7-nodejs-testes-unitarios-integracao/test/folhaDePagamento.test.js
+++ aula7-nodejs-testes-unitarios-integracao/test/folhaDePagamento.test.js
@@ -1,18 +1,23 @@
 // eslint-disable-next-line import/extensions
 import { somaHorasExtras, calculaDescontos } from '../index.js';
 
+// eslint-disable-next-line no-undef
 describe('Testes dos cálculos de folha', () => {
+  // eslint-disable-next-line no-undef
   test('Deve retornar a soma das horas extras', () => {
     const esperado = 2500;
     const retornado = somaHorasExtras(2000, 500);
 
+    // eslint-disable-next-line no-undef
     expect(retornado).toBe(esperado);
   });
 
+  // eslint-disable-next-line no-undef
   it('Deve descontar o valor do salário', () => {
     const esperado = 2300;
     const retornado = calculaDescontos(2500, 200);
 
+    // eslint-disable-next-line no-undef
     expect(retornado).toBe(esperado);
   });
 });
